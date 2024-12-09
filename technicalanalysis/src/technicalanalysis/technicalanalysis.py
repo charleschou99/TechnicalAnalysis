e7b70a4c66fdeeba972f48a1d970ffdf833fd420
charleschou99 <charles.chou@hotmail.fr>
2024-12-09 17:06:07 +0100

    initiate some technicalanalysis functions

--- technicalanalysis/src/technicalanalysis/technicalanalysis.py
+++ technicalanalysis/src/technicalanalysis/technicalanalysis.py
@@ -2,3 +2,35 @@
 Functions to transform time series of OHLCV
 """
 
+def SMA(window:int=14):
+    """
+    Simple moving average
+    window int: size of the rolling window, number of rows given the dataframe
+    """
+    pass
+
+
+def EMA(window:int=14):
+    """
+    Simple moving average
+    window int: size of the rolling window, number of rows given the dataframe
+    """
+    pass
+
+
+def realised_volatility(window:int=14, model:str="CloseToClose"):
+
+    """
+    Rolling realised volatility
+    window int: size of the rolling window, number of rows given the dataframe
+    model str: formula used for the realised volatility
+    """
+
+
+def RSI(window:int=14):
+    """
+    Relative strength index
+    window int: size of the rolling window, number of rows given the dataframe
+    """
+    pass
+
